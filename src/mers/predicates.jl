989605f21e65242d59f53830c1f8bf69120a073a
Ben J. Ward <benjward@protonmail.com>
2020-12-06 01:51:44 +0000

    Adjust Base.hash for Kmers

--- src/mers/predicates.jl
+++ src/mers/predicates.jl
@@ -6,13 +6,6 @@ Base.cmp(x::T, y::T) where {T<:Kmer} = cmp(x.data, y.data)
 Base.:(==)(x::T, y::T) where {T<:Kmer} = x.data == y.data
 Base.isless(x::T, y::T) where {T<:Kmer} = isless(x.data, y.data)
 
-function Base.hash(x::Kmer{A,K,N}, h::UInt) where {A<:NucleicAcidAlphabet{2},K,N}
-    #TODO: Plz review this as I honestly don't know about the behaviour of this hash:
-    # does it have some specific desired properties regarding the values computed by the
-    # hash function? My punt at a solution is just to do the operations over every word.
-    # I believe this is the right thing to do as I reason this should basically produce
-    # the same behaviour (assembly.... at least on my MBP) when N = 2 as when we used
-    # to have UInt128 based big kmers.
-    #return Base.hash(packed_data(x) ⊻ K ⊻ h)
-    return Base.hash(map(xx -> xx ⊻ K ⊻ h, x.data))
-end
+# TODO: Ensure this is the right way to go.
+# See https://github.com/BioJulia/BioSequences.jl/pull/121#discussion_r475234270
+Base.hash(x::Kmer{A,K,N}, h::UInt) where {A,K,N} = hash(x.data, h ⊻ K)
